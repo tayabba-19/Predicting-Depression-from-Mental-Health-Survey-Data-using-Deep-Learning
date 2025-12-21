3f80440c1b8ae867af36e5b9a8b1133c4920ddf6
tayabba-19 <tayabbazabeen@gmail.com>
2025-12-21 23:21:24 +0530

    Update app.py
    about the streamlit app

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import streamlit as st
 
 st.title("Predicting Depression from Mental Health Survey Data")
 
-csv_url = "
+csv_url = "https://raw.githubusercontent.com/tayabba-19/Predicting-Depression-from-Mental-Health-Survey-Data-using-Deep-Learning/refs/heads/main/mental_health_survey.csv"
+df = pd.read_csv(csv_url)
+
+st.success("Dataset loaded successfully!")
+st.dataframe(df.head())
         
